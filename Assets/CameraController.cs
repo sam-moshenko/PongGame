e7d6f2e0cf17ec873c0a8f66f29201892d54fd18
Simon <sam_moshenko@mail.ru>
2016-10-20 15:40:07 +0600

    Errors fixed

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,15 +7,15 @@ public class CameraController : MonoBehaviour {
 	public Text secondPlayerScoreText;
 
 	public void HideScore() {
-		firstPlayerScoreText.color.a = 0;
-		secondPlayerScoreText.color.a = 0;
+//		firstPlayerScoreText.color.a = 0;
+//		secondPlayerScoreText.color.a = 0;
 	}
 
 	public void ShowScore() {
-		for (float i = 0; i < 1.0f; i += 0.1) {
-			firstPlayerScoreText.color.a = i;
-			secondPlayerScoreText.color.a = i;
-			 return
-		}
+//		for (float i = 0; i < 1.0f; i += 0.1) {
+//			firstPlayerScoreText.color.a = i;
+//			secondPlayerScoreText.color.a = i;
+//			 return
+//		}
 	}
 }
